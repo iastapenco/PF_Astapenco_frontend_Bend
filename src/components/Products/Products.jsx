d8bd05adc90f6b3c7b790564d9d890b8169a6816
Iván Astapenco <iastapenco@gmail.com>
2024-01-21 10:57:50 -0300

    se mejora Products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -12,26 +12,27 @@ const Products = () => {
       });
   }, []);
 
-  return;
-  <>
-    <div>
-      <h2 className="center">Lista de productos</h2>
-    </div>
-    <div className="container row-gap-3 column-gap-3 d-flex flex-row justify-content-around align-content-around flex-wrap bg-primary mb-4">
-      {products &&
-        products.mensaje &&
-        products.mensaje.docs &&
-        products.mensaje.docs.map((product) => {
-          return (
-            <div key={product._id} className="product_container ">
-              <div className="card">
-                <CardProducts data={product} />
+  return (
+    <>
+      <div>
+        <h2 className="center">Lista de productos</h2>
+      </div>
+      <div className="container row-gap-3 column-gap-3 d-flex flex-row justify-content-around align-content-around flex-wrap bg-primary mb-4">
+        {products &&
+          products.mensaje &&
+          products.mensaje.docs &&
+          products.mensaje.docs.map((product) => {
+            return (
+              <div key={product._id} className="product_container ">
+                <div className="card">
+                  <CardProducts data={product} />
+                </div>
               </div>
-            </div>
-          );
-        })}
-    </div>
-  </>;
+            );
+          })}
+      </div>
+    </>
+  );
 };
 
 export default Products;
