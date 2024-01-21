8ebd790fa7e8da99f9bd064c08a8dc0d75040e1f
Iván Astapenco <iastapenco@gmail.com>
2024-01-20 21:26:46 -0300

    add LisItem

--- src/components/ListItemContainer/ListItemContainer.jsx
+++ src/components/ListItemContainer/ListItemContainer.jsx
@@ -4,23 +4,25 @@ import "./list_item_container.css";
 const ListItemContainer = () => {
   const user = JSON.parse(localStorage("dataUser"));
 
-  if (user.rol === "admin") {
-    return (
-      <>
-        <h2 className="saludos">
-          {`Bienvenido administrador ${user.first_name} ${user.last_name}`}
-        </h2>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <h2 className="saludos">
-          {`Bienvenido ${user.first_name} ${user.last_name} a nuestro coffee shop online!`}
-        </h2>
-      </>
-    );
-  }
+  if (user) {
+    if (user.rol === "admin") {
+      return (
+        <>
+          <h2 className="saludos">
+            {`Bienvenido administrador ${user.first_name} ${user.last_name}`}
+          </h2>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <h2 className="saludos">
+            {`Bienvenido ${user.first_name} ${user.last_name} a nuestro coffee shop online!`}
+          </h2>
+        </>
+      );
+    }
+  } else null;
 };
 
 export default ListItemContainer;
