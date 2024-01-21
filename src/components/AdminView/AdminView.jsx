942d4fa6b516cd75d4584b9f1630d2784fe284f7
Iván Astapenco <iastapenco@gmail.com>
2024-01-21 11:49:53 -0300

    se soluciona boton

--- src/components/AdminView/AdminView.jsx
+++ src/components/AdminView/AdminView.jsx
@@ -14,7 +14,7 @@ const AdminView = () => {
     return (
       <>
         <button className="btn btn-primary" onClick={toggleUsersList}>
-          {!showUsersList ? "Ocultar usuarios":"ver usuarios"}
+          {showUsersList ? "Ocultar usuarios" : "ver usuarios"}
         </button>
         {showUsersList && <UsersList />}
       </>
