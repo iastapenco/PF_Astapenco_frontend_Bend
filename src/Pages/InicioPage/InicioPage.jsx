80e3bc1d8f9853a7e64883bf310e641326059d2a
Iván Astapenco <iastapenco@gmail.com>
2024-01-21 10:34:24 -0300

    add products component

--- src/Pages/InicioPage/InicioPage.jsx
+++ src/Pages/InicioPage/InicioPage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import CardProducts from "../../components/CardProducts/CardProducts";
+import Products from "../../components/Products/Products";
 import ListItemContainer from "../../components/ListItemContainer/ListItemContainer";
 import Spinner from "../../components/Spinner/Spinner";
 import LoginAndRegister from "../../components/LoginAndRegister/LoginAndRegister";
@@ -7,18 +7,7 @@ import AdminView from "../../components/AdminView/AdminView";
 import "./inicio.css";
 
 function InicioPage() {
-  const [products, setProducts] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    fetch("https://appcoffee-deploy1.onrender.com/api/products")
-      .then((response) => response.json())
-      .then((data) => {
-        setProducts(data);
-        setIsLoading(false);
-      });
-  }, []);
-
   const user = JSON.parse(localStorage.getItem("dataUser"));
 
   if (isLoading) {
@@ -30,23 +19,7 @@ function InicioPage() {
         <>
           <LoginAndRegister />
           <ListItemContainer />
-          <div>
-            <h2 className="center">Lista de productos</h2>
-          </div>
-          <div className="container row-gap-3 column-gap-3 d-flex flex-row justify-content-around align-content-around flex-wrap bg-primary mb-4">
-            {products &&
-              products.mensaje &&
-              products.mensaje.docs &&
-              products.mensaje.docs.map((product) => {
-                return (
-                  <div key={product._id} className="product_container ">
-                    <div className="card">
-                      <CardProducts data={product} />
-                    </div>
-                  </div>
-                );
-              })}
-          </div>
+          <Products />
         </>
       ) : (
         <>
