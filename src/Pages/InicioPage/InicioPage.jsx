fb6aca5f7e69b109995a92e5b6587a3b57ec04bb
Iván Astapenco <iastapenco@gmail.com>
2024-01-21 01:20:42 -0300

    add AdminView

--- src/Pages/InicioPage/InicioPage.jsx
+++ src/Pages/InicioPage/InicioPage.jsx
@@ -3,6 +3,7 @@ import CardProducts from "../../components/CardProducts/CardProducts";
 import ListItemContainer from "../../components/ListItemContainer/ListItemContainer";
 import Spinner from "../../components/Spinner/Spinner";
 import LoginAndRegister from "../../components/LoginAndRegister/LoginAndRegister";
+import AdminView from "../../components/AdminView/AdminView";
 import "./inicio.css";
 
 function InicioPage() {
@@ -27,23 +28,29 @@ function InicioPage() {
     <>
       <LoginAndRegister />
       <ListItemContainer />
-      <div>
-        <h2 className="center">Lista de productos</h2>
-      </div>
-      <div className="container row-gap-3 column-gap-3 d-flex flex-row justify-content-around align-content-around flex-wrap bg-primary mb-4">
-        {products &&
-          products.mensaje &&
-          products.mensaje.docs &&
-          products.mensaje.docs.map((product) => {
-            return (
-              <div key={product._id} className="product_container ">
-                <div className="card">
-                  <CardProducts data={product} />
-                </div>
-              </div>
-            );
-          })}
-      </div>
+      if (user && user.rol !="admin")
+      {
+        <>
+          <div>
+            <h2 className="center">Lista de productos</h2>
+          </div>
+          <div className="container row-gap-3 column-gap-3 d-flex flex-row justify-content-around align-content-around flex-wrap bg-primary mb-4">
+            {products &&
+              products.mensaje &&
+              products.mensaje.docs &&
+              products.mensaje.docs.map((product) => {
+                return (
+                  <div key={product._id} className="product_container ">
+                    <div className="card">
+                      <CardProducts data={product} />
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        </>
+      }
+      else {<AdminView />}
     </>
   );
 }
---
+++ src/components/AdminView/AdminView.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+const AdminView = () => {
+  const [users, setUsers] = useState(null);
+  useEffect(() => {
+    fetch("https://appcoffee-deploy1.onrender.com/api/users/userlist")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <div className="container">
+      {users &&
+        users.mensaje &&
+        users.mensaje.map((user) => {
+          return;
+          <ul key={user._id} className="list-group">
+            <li className="list-group-item">
+              `Usuario: ${user.first_name} ${user.last_name}`
+            </li>
+            <li className="list-group-item">`Email: ${user.email}`</li>
+            <li className="list-group-item">`Rol: ${user.rol}`</li>
+          </ul>;
+        })}
+    </div>
+  );
+};
+
+export default AdminView;
