a49fb0507489ca435b99cfbb2c7c60cc5eebc77d
Iván Astapenco <iastapenco@gmail.com>
2024-01-25 14:16:23 -0300

    change rol

--- src/components/ChangeUserRol/ChangeUserRol.jsx
+++ src/components/ChangeUserRol/ChangeUserRol.jsx
@@ -6,11 +6,27 @@ const ChangeUserRol = ({ dataUser }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const rol = new FormData(formRef.current).get("rol");
+    const datForm = new FormData(formRef.current);
+    const dataForm = Object.fromEntries(datForm);
+    const rol = dataForm.rol;
+    console.log("Rol seleccionado: ", rol);
+
     const token = document.cookie
       .split("; ")
       .find((row) => row.startsWith("jwtCookie="))
       .split("=")[1];
+
+    const dataToSend = {
+      _id,
+      first_name,
+      last_name,
+      age,
+      email,
+      password,
+      rol,
+    };
+
+    console.log("Datos enviados: ", dataToSend); // Imprime los datos enviados
 
     fetch(`https://appcoffee-deploy1.onrender.com/api/users/${_id}`, {
       method: "PUT",
@@ -18,15 +34,7 @@ const ChangeUserRol = ({ dataUser }) => {
         "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
       },
-      body: JSON.stringify({
-        _id,
-        first_name,
-        last_name,
-        age,
-        email,
-        password,
-        rol,
-      }),
+      body: JSON.stringify(dataToSend),
     });
   };
 
