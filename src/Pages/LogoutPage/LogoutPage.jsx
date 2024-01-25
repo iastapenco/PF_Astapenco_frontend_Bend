17bd5cd7fdd4192f31a1b61c20bd66d371bc5d9e
Iván Astapenco <iastapenco@gmail.com>
2024-01-25 16:47:48 -0300

    se mejora codigo en Logout

--- src/Pages/LogoutPage/LogoutPage.jsx
+++ src/Pages/LogoutPage/LogoutPage.jsx
@@ -2,9 +2,19 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const LogoutPage = () => {
+  const token = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("jwtCookie="))
+    .split("=")[1];
   const inicioNavigate = useNavigate();
   const logout = () => {
-    fetch("https://appcoffee-deploy1.onrender.com/api/sessions/logout");
+    fetch("https://appcoffee-deploy1.onrender.com/api/sessions/logout", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
     localStorage.removeItem("dataUser");
     document.cookie =
       "jwtCookie=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=https://appcoffee-deploy1.onrender.com";
