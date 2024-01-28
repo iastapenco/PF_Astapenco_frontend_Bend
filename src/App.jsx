dacf8b79c1e71b4a593ccdb6e1f8e67f7abfc024
Iván Astapenco <iastapenco@gmail.com>
2024-01-28 13:42:19 -0300

    add CartProvider

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,19 @@ import { Login } from "./components/Login/Login";
 import InicioPage from "./Pages/InicioPage/InicioPage";
 import Register from "./components/Register/Register";
 import LogoutPage from "./Pages/LogoutPage/LogoutPage";
+import { ShoppingCartProvider } from "./Context/CartContext";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<InicioPage />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/logout" element={<LogoutPage />} />
-      </Routes>
+      <ShoppingCartProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<InicioPage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/logout" element={<LogoutPage />} />
+        </Routes>
+      </ShoppingCartProvider>
     </>
   );
 }
