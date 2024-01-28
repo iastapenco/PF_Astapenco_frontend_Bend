616db9fa92034194e6b5dc712db93c50e1855b65
Iván Astapenco <iastapenco@gmail.com>
2024-01-28 16:52:41 -0300

    probando otra vez con mode no cors

--- src/components/AddCart/AddCart.jsx
+++ src/components/AddCart/AddCart.jsx
@@ -22,6 +22,7 @@ const AddCart = ({ data }) => {
     const response = await fetch(
       `https://appcoffee-deploy1.onrender.com/api/carts/${dataUser.cart}/products/${_id}`,
       {
+        mode: "no-cors",
         method: "POST",
         headers: {
           "Content-Type": "application/json",
